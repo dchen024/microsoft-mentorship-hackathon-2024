05b630890f784272de778d759b063d2c4389e2f0
Daniel Chen <113078548+dchen024@users.noreply.github.com>
2024-07-30 10:39:54 -0400

    feat(app): add custom avatars

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ st.title("FinFolio")
 
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
+
+def get_avatar(role):
+    return "person.png" if role == "user" else "finfolio.png"
+
+
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-4o-mini"
 
@@ -12,12 +17,14 @@ if "messages" not in st.session_state:
     st.session_state.messages = []
 
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
+    avatar = "person.png" if message["role"] == "user" else "finfolio.png"
+
+    with st.chat_message(message["role"], avatar=avatar):
         st.markdown(message["content"])
 
 if prompt := st.chat_input("Ask a question"):
     st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
+    with st.chat_message("user", avatar="person.png"):
         st.markdown(prompt)
 
     # Prepare the context with the system message
@@ -26,7 +33,7 @@ if prompt := st.chat_input("Ask a question"):
         for m in st.session_state.messages
     ]
 
-    with st.chat_message("assistant"):
+    with st.chat_message("assistant", avatar="finfolio.png"):
         stream = client.chat.completions.create(
             model=st.session_state["openai_model"],
             messages=context,
@@ -34,5 +41,6 @@ if prompt := st.chat_input("Ask a question"):
             temperature=0
         )
         response = st.write_stream(stream)
+
     st.session_state.messages.append(
         {"role": "assistant", "content": response})
